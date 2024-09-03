8adb89f72c44c2a1d4c4afbe957419f9896b3f70
Gabriel C. Netto <gabrielcarlosnetto@outlook.com>
2024-09-03 16:22:12 -0300

    ok

--- lib/usuarioGerente/telas/agendEaddScreen.dart/editarAgendamento/editarAgendamento.dart
+++ lib/usuarioGerente/telas/agendEaddScreen.dart/editarAgendamento/editarAgendamento.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:jimy/DadosGeralApp.dart';
 import 'package:jimy/usuarioGerente/classes/CorteClass.dart';
 
@@ -14,20 +15,379 @@ class EditarAgendamento extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.1,
-              color: Dadosgeralapp().tertiaryColor,
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.1,
+              left: 0,
+              right: 0,
+              child: Container(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 15,
+                      right: 15,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        //container da foto - inicio
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                width: 0.2,
+                                color: Colors.black38,
+                              ),
+                            ),
+                          ),
+                          padding: EdgeInsets.only(top: 10, bottom: 20),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(60),
+                                ),
+                                width: MediaQuery.of(context).size.width * 0.11,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.06,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(60),
+                                  child: Image.network(
+                                    corte.urlImagePerfilfoto,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                "${corte.clienteNome}",
+                                style: GoogleFonts.openSans(
+                                  textStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        //container da foto - fim
+                        SizedBox(
+                          height: 15,
+                        ),
+                        //container do preco - incio
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                width: 0.2,
+                                color: Colors.black38,
+                              ),
+                            ),
+                          ),
+                          padding: EdgeInsets.only(top: 10, bottom: 20),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Valor",
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                "R\$${corte.valorCorte.toStringAsFixed(2).replaceAll('.', ',')}",
+                                style: GoogleFonts.openSans(
+                                  textStyle: TextStyle(
+                                    color: Colors.green.shade600,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        //container do preco - incio
+                        SizedBox(
+                          height: 15,
+                        ),
+                        //container do serviço
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                width: 0.2,
+                                color: Colors.black38,
+                              ),
+                            ),
+                          ),
+                          padding: EdgeInsets.only(top: 10, bottom: 20),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Serviço",
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      "${corte.nomeServicoSelecionado}",
+                                      style: GoogleFonts.openSans(
+                                        textStyle: TextStyle(
+                                          color: Colors.grey.shade400,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Container(
+                                    alignment: Alignment.center,
+                                    child: Icon(
+                                      Icons.edit,
+                                      color: Colors.grey.shade600,
+                                      size: 15,
+                                    ),
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.shade200,
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        //container do servico
+                        //container do profissional
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                width: 0.2,
+                                color: Colors.black38,
+                              ),
+                            ),
+                          ),
+                          padding: EdgeInsets.only(top: 10, bottom: 20),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Profissional",
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      "${corte.ProfissionalSelecionado}",
+                                      style: GoogleFonts.openSans(
+                                        textStyle: TextStyle(
+                                          color: Colors.grey.shade400,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Container(
+                                    alignment: Alignment.center,
+                                    child: Icon(
+                                      Icons.edit,
+                                      color: Colors.grey.shade600,
+                                      size: 15,
+                                    ),
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.shade200,
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        //container do profissional
+                        SizedBox(
+                          height: 15,
+                        ),
+                        //container data e hora
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                width: 0.2,
+                                color: Colors.black38,
+                              ),
+                            ),
+                          ),
+                          padding: EdgeInsets.only(top: 10, bottom: 20),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Data e hora",
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      "${corte.diaSelecionado} de ${corte.MesSelecionado} - ${corte.horarioSelecionado}",
+                                      style: GoogleFonts.openSans(
+                                        textStyle: TextStyle(
+                                          color: Colors.grey.shade400,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Container(
+                                    alignment: Alignment.center,
+                                    child: Icon(
+                                      Icons.edit,
+                                      color: Colors.grey.shade600,
+                                      size: 15,
+                                    ),
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.shade200,
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        //container data e hora
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
-            SizedBox(
-              height: 5,
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.085,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 1,
+                      color: Colors.black26,
+                    ),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 25,
+                        color: Dadosgeralapp().primaryColor,
+                      ),
+                    ),
+                    Text(
+                      "Comanda nº ${corte.id.replaceAll('.', '').substring(0, 5)}",
+                      style: GoogleFonts.poppins(
+                        textStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Container()
+                  ],
+                ),
+              ),
             ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [],
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                elevation: 5,
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 15,horizontal: 15),
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    border: Border(
+                      top: BorderSide(
+                        width: 0.2,
+                        color: Colors.black26,
+                      ),
+                    ),
+                  ),
+                  child: Row(children: [],),
                 ),
               ),
             ),
